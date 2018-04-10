910f8664fae20dd93be542b9f75b7b57a83e0b5f
Li Ge <gelber.libi@gmail.com>
2018-04-10 08:43:23 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -7,10 +7,12 @@ function forLoop(array) {
 }
 
 
-// function whileLoop(n) {
-//   for (var i = n; i <= n && i > -1; i++) {
-//     i -= 1;
-//   }
-//   return "done";
-//
-// }
+function whileLoop(n) {
+  for (var i = n; i <= n; i++) {
+    while (i > -1) {
+      i -= 1;
+    }
+  }
+  return "done";
+
+}
